4013ca45388a9bc1c8ca822ba09b551886857e31
willrnch <1873880+willrnch@users.noreply.github.com>
2025-10-28 10:43:13 +0100

    update dependencies

--- src/secure_random.rs
+++ src/secure_random.rs
@@ -1,7 +1,5 @@
 use std::sync::Once;
-
-use getrandom::getrandom;
-use rand::{CryptoRng, RngCore, SeedableRng, rngs::StdRng};
+use rand::{rngs::StdRng, CryptoRng, RngCore, SeedableRng};
 
 use crate::RandomNumberGenerator;
 
@@ -22,7 +20,7 @@ impl LazyStdRng {
     fn get_rng(&self) -> std::sync::MutexGuard<'_, Option<StdRng>> {
         self.init.call_once(|| {
             let mut seed = [0u8; 32];
-            getrandom(&mut seed).expect("Failed to seed RNG");
+            getrandom::fill(&mut seed).expect("Failed to seed RNG");
             let rng = StdRng::from_seed(seed);
 
             let mut guard = self.rng.lock().expect("Mutex was poisoned");
@@ -70,11 +68,8 @@ impl RngCore for SecureRandomNumberGenerator {
 
     fn next_u64(&mut self) -> u64 { next_u64() }
 
-    fn fill_bytes(&mut self, dest: &mut [u8]) { fill_random_data(dest); }
-
-    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), rand::Error> {
-        self.fill_bytes(dest);
-        Ok(())
+    fn fill_bytes(&mut self, dest: &mut [u8]) {
+        fill_random_data(dest);
     }
 }
 
